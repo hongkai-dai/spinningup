eb83539b545541d1699e0b74eadaeffd0711014d
Hongkai Dai <daihongkai@gmail.com>
2024-10-13 08:48:31 -0700

    vpg_cost takes a batch, not a trajectory.

--- spinup/algos/pytorch/vpg/my_vpg.py
+++ spinup/algos/pytorch/vpg/my_vpg.py
@@ -1,8 +1,11 @@
 from typing import List, Optional, Tuple
 
+import gym
 import numpy as np
+import tqdm
 import torch
 import torch.distributions
+from torch.utils.data import TensorDataset, DataLoader
 
 
 class Actor(torch.nn.Module):
@@ -66,6 +69,32 @@ class Critic(torch.nn.Module):
     def forward(self, obs: torch.Tensor, *, t: torch.Tensor = None) -> torch.Tensor:
         raise NotImplementedError
 
+    def _to_dataset(
+        self, obs: torch.Tensor, gamma: float, rew: torch.Tensor
+    ) -> TensorDataset:
+        raise NotImplementedError
+
+    def train(
+        self,
+        obs: torch.Tensor,
+        rew: torch.Tensor,
+        gamma: float,
+        optimizer: torch.optim.Optimizer,
+        num_epochs: int = 100,
+        batch_size: int = 128,
+    ):
+        adv_dataset = self._to_dataset(obs, gamma, rew)
+
+        loader = DataLoader(adv_dataset, batch_size=batch_size)
+        for epoch in tqdm.tqdm(range(num_epochs)):
+            for training_data in loader:
+                x, t, adv_target = training_data
+                adv = self(x, t=t)
+                loss = torch.nn.MSELoss()(adv, adv_target)
+                loss.backward()
+                optimizer.step()
+                optimizer.zero_grad()
+
 
 class MLPCritic(Critic):
     def __init__(
@@ -89,38 +118,64 @@ class MLPCritic(Critic):
             mlp_input = obs
         return self.mlp(mlp_input)
 
+    def _to_dataset(
+        self, obs: torch.Tensor, gamma: float, rew: torch.Tensor, device="cuda"
+    ) -> TensorDataset:
+        batch_size = obs.shape[0]
+        rtg = compute_rtg(gamma, rew).to(device)
+        T = rew.shape[1]
+        t = torch.arange(T).to(torch.float).to(devie)
+        t_batch = t.unsqueeze(0).repeat((batch_size, 1))
+        return TensorDataset(obs, t_batch, rtg)
+
+
+def compute_rtg(gamma: float, rew_path: torch.Tensor) -> torch.Tensor:
+    """
+    Compute reward-to-go ∑ₜ₋ₚᵣᵢₘₑ γᵗ⁻ᵖʳⁱᵐᵉ*rₜ₋ₚᵣᵢₘₑ
+
+    Args:
+      rew_path: Size is (batch_size, T)
+    Return:
+      rtg: Size is (batch_size, T)
+    """
+    T = rew_path.shape[1]
+    t = torch.arange(0, T).to(torch.float)
+    discounted_rew = torch.pow(gamma, t) * rew_path
+    # cum_rew is of size (batch_size, T)
+    cum_rew = discounted_rew.flip(1).cumsum(1).flip(1)
+    return cum_rew
+
 
 def vpg_cost(
-    actor: Actor,
     obs: torch.Tensor,
     act: torch.Tensor,
     log_pi: torch.Tensor,
-    rew: torch.Tensor,
+    rtg: torch.Tensor,
+    t: torch.Tensor,
     critic: Critic,
-    gamma: float,
     time_varying: bool,
 ):
     """
     Compute the term ∑ₜlog π(aₜ|sₜ) * Gₜ
     where Gₜ is the advantage function ∑ₜ₋ₚᵣᵢₘₑ γᵗ⁻ᵖʳⁱᵐᵉ*rₜ₋ₚᵣᵢₘₑ − V(sₜ, t)
-    obs: (batch_size, T, obs_dim)
-    act: (batch_size, T, act_dim)
-    rew: (batch_size, T)
+    obs: (batch_size, obs_dim)
+    act: (batch_size, act_dim)
+    rtg: (batch_size,), reward-to-go ∑ₜ₋ₚᵣᵢₘₑ γᵗ⁻ᵖʳⁱᵐᵉ*rₜ₋ₚᵣᵢₘₑ
+    t:   (batch_size,)
     """
     batch_size = obs.shape[0]
-    T = obs.shape[1]
-    assert act.shape[0] == rew.shape[0] == batch_size
-    assert act.shape[1] == rew.shape[1] == T
-    assert log_pi.shape == (batch_size, T)
-    assert rew.shape == (batch_size, T)
-    assert len(rew.shape) == 2
-    assert gamma > 0 and gamma <= 1
-    t = torch.arange(0, T).to(torch.float)
-    discounted_rew = torch.pow(gamma, t) * rew
-    # cum_rew is of size (batch_size, T)
-    cum_rew = discounted_rew.flip(1).cumsum(1).flip(1)
-    obs_flat = obs.view((batch_size * T, -1))
-    t_flat = t.unsqueeze(0).repeat((batch_size, 1)).view((-1,))
-    V_val = critic(obs_flat, t=t_flat if time_varying else None).view((batch_size, T))
-    cost = (log_pi * (cum_rew - V_val)).sum()
+    assert act.shape[0] == rtg.shape[0] == batch_size
+    assert log_pi.shape == (batch_size,)
+    assert rtg.shape == (batch_size,)
+    assert t.shape == (batch_size,)
+    V_val = critic(obs, t=t if time_varying else None)
+    cost = (log_pi * (rtg - V_val)).sum()
     return cost
+
+
+class VPGBuffer:
+    def __init__(self, T: int, obs_dim: int, act_dim: int, buffer_size: int):
+        self.obs_buffer = np.zeros((buffer_size, T, obs_dim))
+        self.act_buffer = np.zeros((buffer_size, T, act_dim, T))
+        self.rew_buffer = np.zeros((buffer_size, T))
+        self.logp_buffer = np.zeros((buffer_size, T))
--- test/test_my_vpg.py
+++ test/test_my_vpg.py
@@ -39,61 +39,56 @@ class TestVPG:
             time_varying=False,
         )
 
+    def test_compute_rtg(self):
+        batch_size = 4
+        T = 10
+        rew_path = torch.randn((batch_size, T))
+        gamma = 0.95
+        rtg = mut.compute_rtg(gamma, rew_path)
+        assert rtg.shape == (batch_size, T)
+        for i in range(batch_size):
+            np.testing.assert_almost_equal(
+                rtg[i, -1], gamma ** (T - 1) * rew_path[i, -1], decimal=5
+            )
+            for j in range(T - 1, 0, -1):
+                np.testing.assert_almost_equal(
+                    rtg[i, j - 1],
+                    rtg[i, j] + gamma ** (j - 1) * rew_path[i, j - 1],
+                    decimal=5,
+                )
+
     def test_vpg_cost(self):
         batch_size = 8
-        T = 16
-        obs = torch.rand(batch_size, T, self.obs_dim)
-        t = torch.arange(0, T).to(torch.float)
-        (act_ti, log_pi_ti) = self.ti_actor(obs.view((batch_size * T, self.obs_dim)))
-        act_ti = act_ti.view((batch_size, T, self.act_dim))
-        log_pi_ti = log_pi_ti.view((batch_size, T))
-        (act_tv, log_pi_tv) = self.tv_actor(
-            obs.view((batch_size * T, self.obs_dim)), t=t.repeat(batch_size)
-        )
-        act_tv = act_tv.view((batch_size, T, self.act_dim))
-        log_pi_tv = log_pi_tv.view((batch_size, T))
-        rew_ti = ((obs**2).sum(dim=-1) + (act_ti**2).sum(dim=-1)).detach()
-        rew_tv = ((obs**2).sum(dim=-1) + (act_tv**2).sum(dim=-1)).detach()
-        gamma = 0.95
+        obs = torch.rand(batch_size, self.obs_dim)
+        T = 10
+        t = torch.floor(torch.rand((batch_size,)) * T).to(torch.float)
+        (act_ti, log_pi_ti) = self.ti_actor(obs)
+        (act_tv, log_pi_tv) = self.tv_actor(obs, t=t)
+        rtg_ti = torch.rand((batch_size,))
+        rtg_tv = torch.rand((batch_size,))
+
         cost_ti = mut.vpg_cost(
-            self.ti_actor,
             obs,
             act_ti,
             log_pi_ti,
-            rew_ti,
+            rtg_ti,
+            t,
             self.ti_critic,
-            gamma,
             time_varying=False,
         )
         cost_tv = mut.vpg_cost(
-            self.tv_actor,
             obs,
             act_tv,
             log_pi_tv,
-            rew_tv,
+            rtg_tv,
+            t,
             self.tv_critic,
-            gamma,
             time_varying=True,
         )
 
         # Now compute the cost with for loop.
-        t = torch.arange(0, T).to(torch.float)
-        rtg_ti = torch.zeros((batch_size, T))
-        rtg_tv = torch.zeros((batch_size, T))
-        for i in range(batch_size):
-            rtg_ti[i, -1] = rew_ti[i, -1] * gamma ** (T - 1)
-            rtg_tv[i, -1] = rew_tv[i, -1] * gamma ** (T - 1)
-            for j in range(T - 1, 0, -1):
-                rtg_ti[i, j - 1] = rtg_ti[i, j] + rew_ti[i, j - 1] * gamma ** (j - 1)
-                rtg_tv[i, j - 1] = rtg_tv[i, j] + rew_tv[i, j - 1] * gamma ** (j - 1)
-        V_val_ti = torch.zeros((batch_size, T))
-        V_val_tv = torch.zeros((batch_size, T))
-        for i in range(batch_size):
-            for j in range(T):
-                V_val_ti[i, j] = self.ti_critic(obs[i, j])
-                V_val_tv[i, j] = self.tv_critic(
-                    obs[i, j], t=torch.tensor([j], dtype=torch.float)
-                )
+        V_val_ti = self.ti_critic(obs)
+        V_val_tv = self.tv_critic(obs, t=t)
         cost_ti_expected = (log_pi_ti * (rtg_ti - V_val_ti)).sum()
         cost_tv_expected = (log_pi_tv * (rtg_tv - V_val_tv)).sum()
         np.testing.assert_allclose(
